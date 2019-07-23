e1f68d98c6d0ff13dc8b06ec2f858838f937a925
Jenn J Will <jennijwill@gmail.com>
2019-07-23 13:54:54 -0700

    basic node app checkpoint completed

---
+++ goodbye.js
@@ -0,0 +1,7 @@
+exports.goodbyeEnglish = function() {
+  console.log("Thanks and goodbye");
+};
+
+exports.goodbyeSpanish = function() {
+  console.log("Gracias y hasta luego");
+};
---
+++ main.js
@@ -0,0 +1,21 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+const goodbyes = require("./goodbye.js");
+
+//Thanks and goodbye
+goodbyes.goodbyeEnglish();
+
+//Gracias & hasta luego
+goodbyes.goodbyeSpanish();
